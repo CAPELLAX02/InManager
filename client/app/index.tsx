5cb39844ea3ca87634fe5a8956d51da0bcda8df1
CAPELLAX02 <ahmtatar@gmail.com>
2024-09-18 16:52:34 +0300

    project cleanup

---
+++ client/app/index.tsx
@@ -0,0 +1,23 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+import { Link } from 'expo-router';
+import Colors from '@/constants/Colors';
+
+export default function index() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Index Page of the App</Text>
+      <Link
+        href=""
+        style={{
+          padding: 16,
+          margin: 8,
+          backgroundColor: Colors.PRIMARY,
+          color: Colors.BLACK,
+        }}
+      >
+        Go to (tabs)/index
+      </Link>
+    </View>
+  );
+}
